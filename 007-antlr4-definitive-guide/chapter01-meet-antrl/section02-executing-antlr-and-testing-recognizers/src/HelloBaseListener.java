955033bffda2204ba083feb611cd97868171c9c3
王俊超 <shining-glory@qq.com>
2020-01-05 08:46:44 +0800

    commit
    Change-Id: I4b566ccb3c76bea9bd3cba719c0f83e0c3abef0e

---
+++ 007-antlr4-definitive-guide/chapter01-meet-antrl/section02-executing-antlr-and-testing-recognizers/src/HelloBaseListener.java
@@ -0,0 +1,16 @@
+// Generated from /Users/wangjunchao/IdeaProjects/Java/007-antlr4权威指南/第1章-初识antrl4/1.2-运行ANTLR并测试识别程序/Hello.g4 by ANTLR 4.0
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.antlr.v4.runtime.tree.ErrorNode;
+
+public class HelloBaseListener implements HelloListener {
+	@Override public void enterR(HelloParser.RContext ctx) { }
+	@Override public void exitR(HelloParser.RContext ctx) { }
+
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	@Override public void visitTerminal(TerminalNode node) { }
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ 007-antlr4-definitive-guide/chapter01-meet-antrl/section02-executing-antlr-and-testing-recognizers/src/HelloListener.java
@@ -0,0 +1,8 @@
+// Generated from /Users/wangjunchao/IdeaProjects/Java/007-antlr4权威指南/第1章-初识antrl4/1.2-运行ANTLR并测试识别程序/Hello.g4 by ANTLR 4.0
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.Token;
+
+public interface HelloListener extends ParseTreeListener {
+	void enterR(HelloParser.RContext ctx);
+	void exitR(HelloParser.RContext ctx);
+}
