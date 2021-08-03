58fa8c511795fec5cd74d9f0c079fe896984338e
王俊超 <shining-glory@qq.com>
2021-08-04 06:00:41 +0800

    Spring Security

---
+++ 000-技术笔记/src/spring-cloud.md
@@ -0,0 +1,5 @@
+# OAuth2
+
+Spring Cloud OAuth2 教程
+https://www.jianshu.com/p/3427549a148a
+https://blog.csdn.net/qq_41474114/article/details/89353363
--- 010-spring-security/first-demo/src/main/java/com/wjc/security/cnfig/SecurityCustomConfig.java
+++ 010-spring-security/first-demo/src/main/java/com/wjc/security/cnfig/SecurityCustomConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * <pre>
@@ -25,6 +29,16 @@ public class SecurityCustomConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+//        jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -39,20 +53,29 @@ public class SecurityCustomConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        // 退出
+        http.logout().logoutUrl("/logout")
+                .logoutSuccessUrl("/test/hello").permitAll();
+
+        // 配置没有权限的页面
         http.exceptionHandling().accessDeniedPage("/unauth.html");
 
         http.formLogin() // 自定自己编写的登录页面
                 .loginPage("/login.html") // 登录页面设置
                 .loginProcessingUrl("/user/login") // 登录访问路径
-                .defaultSuccessUrl("/test/index") // 登录成功后，跳转路径
+//                .defaultSuccessUrl("/test/index") // 登录成功后，跳转路径
+                .defaultSuccessUrl("/success.html") // 登录成功后，跳转路径
                 .permitAll()
                 .and().authorizeRequests()
                 .antMatchers("/", "/test/hello", "user/login").permitAll()
                 // admins才能访问
 //                .antMatchers("/test/index").hasAuthority("admins")
 //                .antMatchers("/test/index").hasAnyAuthority("admins", "manager")
-                .antMatchers("/test/index").hasRole("sale1")
+                .antMatchers("/test/index").hasRole("sale")
                 .anyRequest().authenticated()
+                .and().rememberMe().tokenRepository(persistentTokenRepository())
+                .tokenValiditySeconds(60)
+                .userDetailsService(userDetailsService)
                 .and().csrf().disable(); // 关闭csrf
     }
 }
