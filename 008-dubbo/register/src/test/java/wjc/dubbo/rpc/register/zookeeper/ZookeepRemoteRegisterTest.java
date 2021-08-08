ca2a00bfd37dbb0234323f55da3899935d17898a
王俊超 <shining-glory@qq.com>
2021-08-08 16:19:46 +0800

    升级JUnit到5

--- 008-dubbo/register/src/test/java/wjc/dubbo/rpc/register/zookeeper/ZookeepRemoteRegisterTest.java
+++ 008-dubbo/register/src/test/java/wjc/dubbo/rpc/register/zookeeper/ZookeepRemoteRegisterTest.java
@@ -1,23 +1,26 @@
 package wjc.dubbo.rpc.register.zookeeper;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import wjc.dubbo.rpc.api.HelloService;
 import wjc.dubbo.rpc.api.entity.URL;
-import org.junit.Before;
 
 public class ZookeepRemoteRegisterTest {
     ZookeepRemoteRegister remoteRegister;
-    @Before
+
+    @BeforeEach
     public void setUp() throws Exception {
         remoteRegister = new ZookeepRemoteRegister(new ZookeeperClient());
     }
 
-    @org.junit.Test
+    @Test
     public void register() {
 
-        remoteRegister.register(HelloService.class.getName(),new URL("localhost",8021));
+        remoteRegister.register(HelloService.class.getName(), new URL("localhost", 8021));
     }
 
-    @org.junit.Test
+
+    @Test
     public void getRadomURL() {
         register();
         remoteRegister.getRadomURL(HelloService.class.getName());
