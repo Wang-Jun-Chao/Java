5f89424532140797c29496e9afa40dcb5a7bb3b9
王俊超 <shining-glory@qq.com>
2021-09-07 22:17:48 +0800

    API方式使用dubbo

---
+++ 008-dubbo/001-dubbo-demo-api/dubbo-demo-api-consumer/src/main/java/wjc/dubbo/d001/api/consumer/Application.java
@@ -0,0 +1,57 @@
+package wjc.dubbo.d001.api.consumer;
+
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.MetadataReportConfig;
+import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.bootstrap.DubboBootstrap;
+import org.apache.dubbo.config.utils.ReferenceConfigCache;
+import org.apache.dubbo.rpc.service.GenericService;
+import wjc.dubbo.demo.api.DemoService;
+
+public class Application {
+    public static void main(String[] args) {
+        if (isClassic(args)) {
+            runWithRefer();
+        } else {
+            runWithBootstrap();
+        }
+    }
+
+    private static boolean isClassic(String[] args) {
+        return args.length > 0 && "classic".equalsIgnoreCase(args[0]);
+    }
+
+    private static void runWithBootstrap() {
+        ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
+        reference.setInterface(DemoService.class);
+        reference.setGeneric("true");
+
+        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
+        bootstrap.application(new ApplicationConfig("dubbo-demo-api-consumer"))
+                .registry(new RegistryConfig("zookeeper://192.168.241.129:2181"))
+                .reference(reference)
+                .start();
+
+        DemoService demoService = ReferenceConfigCache.getCache().get(reference);
+        String message = demoService.sayHello("dubbo");
+        System.out.println(message);
+
+        // generic invoke
+        GenericService genericService = (GenericService) demoService;
+        Object genericInvokeResult = genericService.$invoke("sayHello", new String[] { String.class.getName() },
+                new Object[] { "dubbo generic invoke" });
+        System.out.println(genericInvokeResult);
+    }
+
+    private static void runWithRefer() {
+        ReferenceConfig<DemoService> reference = new ReferenceConfig<>();
+        reference.setApplication(new ApplicationConfig("dubbo-demo-api-consumer"));
+        reference.setRegistry(new RegistryConfig("zookeeper://192.168.241.129:2181"));
+        reference.setMetadataReportConfig(new MetadataReportConfig("zookeeper://192.168.241.129:2181"));
+        reference.setInterface(DemoService.class);
+        DemoService service = reference.get();
+        String message = service.sayHello("dubbo");
+        System.out.println(message);
+    }
+}
---
+++ 008-dubbo/001-dubbo-demo-api/dubbo-demo-api-provider/src/main/java/wjc/dubbo/d001/api/provider/DemoServiceImpl.java
@@ -0,0 +1,25 @@
+package wjc.dubbo.d001.api.provider;
+
+import org.apache.dubbo.rpc.RpcContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import wjc.dubbo.demo.api.DemoService;
+
+import java.util.concurrent.CompletableFuture;
+
+public class DemoServiceImpl implements DemoService {
+    private static final Logger logger = LoggerFactory.getLogger(DemoServiceImpl.class);
+
+    @Override
+    public String sayHello(String name) {
+        logger.info("Hello " + name + ", request from consumer: " + RpcContext.getServiceContext().getRemoteAddress());
+        return "Hello " + name + ", response from provider: " + RpcContext.getServiceContext().getLocalAddress();
+    }
+
+    @Override
+    public CompletableFuture<String> sayHelloAsync(String name) {
+        return null;
+    }
+
+}
